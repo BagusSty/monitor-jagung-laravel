14983787ebd69436dd94fae67f7c4afcfbe78e95
BagusSty <bagussetyo442@gmail.com>
2024-01-23 11:15:00 +0700

    add menu data-produk

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProdukController extends Controller
 {
     public function index() {
-        return view("data-produk");
+        if (Auth::check()) {
+            return view("data-produk");
+        } else {
+            return redirect('login')->with('error','Anda Belum Login');
+        }
     }
 }
